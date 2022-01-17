4847164eeb25dfcd35270bc39830f1ba1ed2f3f2
Antonio <tonito@umich.edu>
2022-01-16 20:13:20 -0500

    Front end connected

--- server.js
+++ server.js
@@ -11,13 +11,11 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
+//serves static files - css and js
+app.use(express.static('public'));
 
 const { animals } = require('./data/animals');
 const { retry } = require('statuses');
-
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-});
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -121,4 +119,24 @@ app.post('/api/animals', (req,res) =>{
 
     res.json(animal);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
